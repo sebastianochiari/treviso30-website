90d953406e088e3815ac68a19d9126cb99fff95b
Sebastiano Chiari <sebastiano.chiari@studenti.unitn.it>
2020-08-21 10:59:04 +0200

    fix links in the single post view to other correlated, recent nand prev/succ posts

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -8,9 +8,25 @@ use App\Post;
 class NewsController extends Controller
 {
     public function getSingle($slug) {
+        // MAIN POST
         // fetch from the DB based on slug (first() -> stop at the first one instead of collection)
         $mainPost = Post::where('slug', '=', $slug)->first();
+
+        // RECENT POSTS
+        // fetch from the DB the 4 most recent posts (except the one that is showed)
+        $recentPosts = Post::orderBy('id','desc')->where('id', '!=', $mainPost->id)->limit(4)->get();
+
+        // CORRELATED POSTS
+        // fetch from the DB the 3 most recent posts from the same category(except the one that is showed)
+        $correlatedPosts = Post::orderBy('id','desc')->where('id', '!=', $mainPost->id)->where('category_id', '=', $mainPost->category_id)->limit(3)->get();
+
+        // NEXT POST
+        $nextPost = Post::orderBy('id','asc')->where('id', '>', $mainPost->id)->first();
+
+        // PREC POST
+        $prevPost = Post::orderBy('id','desc')->where('id', '<', $mainPost->id)->first();
+
         // return the view and pass in the post object
-        return view('news.single')->with('mainPost', $mainPost);
+        return view('news.single')->with('mainPost', $mainPost)->with('recentPosts', $recentPosts)->with('correlatedPosts', $correlatedPosts)->with('nextPost', $nextPost)->with('prevPost', $prevPost);
     }
 }
--- resources/views/news/single.blade.php
+++ resources/views/news/single.blade.php
@@ -2,134 +2,114 @@
 
 @section('stylesheets')
 
-    <!-- Style CSS -->
-    <link rel="stylesheet" href="{{ url('/css/style.css') }}">
+<!-- Style CSS -->
+<link rel="stylesheet" href="{{ url('/css/style.css') }}">
 
 @endsection
 
 @section('title', "- $mainPost->title")
 
 @section('content')
-            <div class="row mt-3">
+<div class="row mt-3">
 
-                <!-- AREA CHE CONTIENE IL POST -->
-                <div class="col-md-8">
-                    <div class="blog-post m-2">
-                        <!-- IMMAGINE -->
-                        <div class="img-container mb-2">
-                            <img src="{{ url('/images/example-img.jpg') }}" alt="" class="post-img">
-                            <div class="bottom-left post-categoria" style="background-color: #{{ $mainPost->category->color }} !important">{{ $mainPost->category->name }}</div>
-                        </div>
-                        <!-- TITOLO -->
-                        <h3 class="post-titolo mt-3 mb-3">
-                            {{ $mainPost->title }}
-                        </h3>
-                        <!-- CATEGORIA + DATA -->
-                        <div class="post-meta">
-                            <div class="post-data">
-                                <p>{{ date('j F Y', strtotime($mainPost->created_at)) }} di Giampaolo Zorzo</p>
-                            </div>
-                        </div>
-                        <!-- CORPO del POST -->
-                        <div class="post-corpo mt-3 mb-3">
-                            {!! $mainPost->body !!}
-                        </div>
-                        <!-- BANNER CONDIVISIONE -->
-                        <div class="post-condivisione mt-5 pt-4 pb-4">
-                        </div>
-                        <!-- POST PRECEDENTE - POST SUCCESSIVO -->
-                        <div class="row mt-5 pt-5 mb-5 pb-5 post-prevsucc">
-                            <div class="col-md-5 text-left">
-                                <div>
-                                    <i class="fas fa-arrow-left mr-2"></i>
-                                    POST PREC
-                                </div>
-                                <div class="mt-3">
-                                    <a href="#">
-                                        Questo porta all'articolo precedente (titolo articolo)
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="col-md-2">
-                                <!-- separatore di servizio -->
-                            </div>
-                            <div class="col-md-5 text-right">
-                                <div>
-                                    POST SUCC
-                                    <i class="fas fa-arrow-right ml-2"></i>
-                                </div>
-                                <div class="mt-3">
-                                    <a href="#">
-                                        Questo porta all'articolo successivo (titolo articolo)
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- RELATED POSTS -->
-                        <div class="related-posts">
-                            <h5 class="post-lateralbanner-postrecenti">POTREBBE INTERESSARTI</h5>
-                            <div class="row mt-4">
-                                <div class="col-md-4">
-                                    <img class="w-100" src="{{ url('/images/example-img.jpg') }}" alt="Card image cap">
-                                    <h6 class="mt-2">In falesia e in montagna, diversi tipi di arrampicata</h6>
-                                    <p class="recent-card-text">Gennaio 1, 2014</p>
-                                </div>
-                                <div class="col-md-4">
-                                    <img class="w-100" src="{{ url('/images/example-img.jpg') }}" alt="Card image cap">
-                                    <h6 class="mt-2">In falesia e in montagna, diversi tipi di arrampicata</h6>
-                                    <p class="recent-card-text">Gennaio 1, 2014</p>
-                                </div>
-                                <div class="col-md-4">
-                                    <img class="w-100" src="{{ url('/images/example-img.jpg') }}" alt="Card image cap">
-                                    <h6 class="mt-2">In falesia e in montagna, diversi tipi di arrampicata</h6>
-                                    <p class="recent-card-text">Gennaio 1, 2014</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- AREA CHE CONTIENE I CORRELATI -->
-                <div class="col-md-4">
-                    <div class="sticky-top pt-5">
-                        <h5 class="post-lateralbanner-postrecenti">POST RECENTI</h5>
-                        <div class="recent-card img-container">
-                            <img class="post-img" src="{{ url('/images/example-img.jpg') }}" alt="">
-                            <div class="bottom-center align-center">
-                                <h6>In falesia e in montagna, diversi tipi di arrampicata</h6>
-                                <p class="recent-card-text">Gennaio 1, 2014</p>
-                            </div>
-                        </div>
-                        <div class="recent-card splitscreen" href="">
-                            <div class="left-1">
-                                <img class="recent-card-side-image" src="{{ url('/images/example-img.jpg') }}" alt="Card image cap">
-                            </div>
-                            <div class="right-2 recent-card-side-text">
-                                <h6>In falesia e in montagna, diversi tipi di arrampicata</h6>
-                                <p class="recent-card-text">Gennaio 1, 2014</p>
-                            </div>
-                        </div>
-                        <div class="recent-card splitscreen">
-                            <div class="left-1">
-                                <img class="recent-card-side-image" src="{{ url('/images/example-img.jpg') }}" alt="Card image cap">
-                            </div>
-                            <div class="right-2 recent-card-side-text">
-                                <h6>In falesia e in montagna, diversi tipi di arrampicata</h6>
-                                <p class="recent-card-text">Gennaio 1, 2014</p>
-                            </div>
-                        </div>
-                        <div class="recent-card splitscreen">
-                            <div class="left-1">
-                                <img class="recent-card-side-image" src="{{ url('/images/example-img.jpg') }}" alt="Card image cap">
-                            </div>
-                            <div class="right-2 recent-card-side-text">
-                                <h6>In falesia e in montagna, diversi tipi di arrampicata</h6>
-                                <p class="recent-card-text">Gennaio 1, 2014</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
+    <!-- AREA CHE CONTIENE IL POST -->
+    <div class="col-md-8">
+        <div class="blog-post m-2">
+            <!-- IMMAGINE -->
+            <div class="img-container mb-2">
+                <img src="{{ url('/images/example-img.jpg') }}" alt="" class="post-img">
+                <div class="bottom-left post-categoria" style="background-color: #{{ $mainPost->category->color }} !important">{{ $mainPost->category->name }}</div>
             </div>
+            <!-- TITOLO -->
+            <h3 class="post-titolo mt-3 mb-3">
+                {{ $mainPost->title }}
+            </h3>
+            <!-- CATEGORIA + DATA -->
+            <div class="post-meta">
+                <div class="post-data">
+                    <p>{{ date('j F Y', strtotime($mainPost->created_at)) }} di Giampaolo Zorzo</p>
+                </div>
+            </div>
+            <!-- CORPO del POST -->
+            <div class="post-corpo mt-3 mb-3">
+                {!! $mainPost->body !!}
+            </div>
+            <!-- BANNER CONDIVISIONE -->
+            <div class="post-condivisione mt-5 pt-4 pb-4">
+            </div>
+            <!-- POST PRECEDENTE - POST SUCCESSIVO -->
+            <div class="row mt-5 pt-5 mb-5 pb-5 post-prevsucc">
+                <div class="col-md-5 text-left">
+                    @if(!blank($prevPost))
+                    <div>
+                        <i class="fas fa-arrow-left mr-2"></i>
+                        POST PREC
+                    </div>
+                    <div class="mt-3">
+                        <h5>
+                            <a class="text-dark" href="{{ route('news.single', $prevPost->slug) }}">
+                                {{ $prevPost->title }}
+                            </a>
+                        </h5>
+                    </div>
+                    @endif
+                </div>
+                <div class="col-md-2">
+                    <!-- separatore di servizio -->
+                </div>
+                <div class="col-md-5 text-right">
+                    @if(!blank($nextPost))
+                    <div>
+                        POST SUCC
+                        <i class="fas fa-arrow-right ml-2"></i>
+                    </div>
+                    <div class="mt-3">
+                        <h5>
+                            <a class="text-dark" href="{{ route('news.single', $nextPost->slug) }}">
+                                {{ $nextPost->title }}
+                            </a>
+                        </h5>
+                    </div>
+                    @endif
+                </div>
+            </div>
+            <!-- RELATED POSTS -->
+            <div class="related-posts">
+                @if(!$correlatedPosts->isEmpty())
+                    <h5 class="post-lateralbanner-postrecenti">POTREBBE INTERESSARTI</h5>
+                    <div class="row mt-4">
+                        @foreach ($correlatedPosts as $post)
+                        <div class="col-md-4">
+                            <a class="text-dark" href="{{ route('news.single', $post->slug) }}">
+                                <img class="w-100" src="{{ url('/images/example-img.jpg') }}" alt="Card image cap">
+                                <h5 class="mt-2">
+                                    {{ $post->title }}
+                                </h5>
+                            </a>
+                        </div>
+                        @endforeach
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+
+    <!-- AREA CHE CONTIENE I RECENTI -->
+    <div class="col-md-4">
+        <div class="sticky-top pt-5">
+            <h5 class="post-lateralbanner-postrecenti">POST RECENTI</h5>
+
+            @foreach ($recentPosts as $post)
+            <div class="recent-card">
+                <div class="box-shadow post-categoria" style="background-color: #{{ $post->category->color }}">{{ $post->category->name }}</div>
+                <h5 class="mt-1">
+                    <a class="text-dark" href="{{ route('news.single', $post->slug) }}">{{ $post->title }}</a>
+                </h5>
+                <p class="recent-card-text">{{ date('j M Y', strtotime($post->created_at)) }}</p>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</div>
 
 @endsection
