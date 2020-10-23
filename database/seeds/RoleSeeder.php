d152e564c3d4e0428a96ed15d9627fa7887c509a
Sebastiano Chiari <sebastiano.chiari@studenti.unitn.it>
2020-10-23 12:19:46 +0200

    real db seed modification

---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::truncate();
+
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'author']);
+    }
+}
